d9ba89ea630233ce6a34b6d58b1a61a10a09d1a1
Chris Granger <ibdknox@gmail.com>
2011-08-13 13:39:44 -0700

    add an example of the data
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- src/lunch_roller/models/places.clj
+++ src/lunch_roller/models/places.clj
@@ -31,3 +31,55 @@
 
 (defn get-all []
   @data)
+
+;;    "businesses": [
+;;    {
+;;      "categories": [
+;;        [
+;;          "Local Flavor",
+;;          "localflavor"
+;;        ],
+;;        [
+;;          "Mass Media",
+;;          "massmedia"
+;;        ]
+;;      ],
+;;      "display_phone": "+1-415-908-3801",
+;;      "id": "yelp-san-francisco",
+;;      "image_url": "http://s3-media2.px.yelpcdn.com/bphoto/7DIHu8a0AHhw-BffrDIxPA/ms.jpg",
+;;      "location": {
+;;        "address": [
+;;          "706 Mission St"
+;;        ],
+;;        "city": "San Francisco",
+;;        "coordinate": {
+;;          "latitude": 37.786138600000001,
+;;          "longitude": -122.40262130000001
+;;        },
+;;        "country_code": "US",
+;;        "cross_streets": "3rd St & Opera Aly",
+;;        "display_address": [
+;;          "706 Mission St",
+;;          "(b/t 3rd St & Opera Aly)",
+;;          "SOMA",
+;;          "San Francisco, CA 94103"
+;;        ],
+;;        "geo_accuracy": 8,
+;;        "neighborhoods": [
+;;          "SOMA"
+;;        ],
+;;        "postal_code": "94103",
+;;        "state_code": "CA"
+;;      },
+;;      "mobile_url": "http://lite.yelp.com/biz/4kMBvIEWPxWkWKFN__8SxQ",
+;;      "name": "Yelp",
+;;      "phone": "4159083801",
+;;      "rating_img_url": "http://media1.px.yelpcdn.com/static/201012161694360749/i/ico/stars/stars_3.png",
+;;      "rating_img_url_large": "http://media3.px.yelpcdn.com/static/201012161053250406/i/ico/stars/stars_large_3.png",
+;;      "rating_img_url_small": "http://media1.px.yelpcdn.com/static/201012162337205794/i/ico/stars/stars_small_3.png",
+;;      "review_count": 3347,
+;;      "snippet_image_url": "http://s3-media2.px.yelpcdn.com/photo/LjzacUeK_71tm2zPALcj1Q/ms.jpg",
+;;      "snippet_text": "Sometimes we ask questions without reading an email thoroughly as many of us did for the last event.  In honor of Yelp, the many questions they kindly...",
+;;      "url": "http://www.yelp.com/biz/yelp-san-francisco"
+;;    }
+;; ]
