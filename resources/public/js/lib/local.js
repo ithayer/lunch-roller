f10f4e611f8e03bc47eb3f5c2453321efed46f5e
Josh Minnich <joshminnich@gmail.com>
2011-08-13 15:02:23 -0700

    Added map markers, vote buttons, and map controls.

--- resources/public/js/lib/local.js
+++ resources/public/js/lib/local.js
@@ -7,9 +7,14 @@ RFZ.page = {};
 // Places model
 RFZ.page.Place = Backbone.Model.extend({
 	id: 0,
-	address: '',
+	categories: [],
 	name: '',
-	yelp: {}
+	image_url: '',
+	location: {},
+	rating: 0,
+	review_count: 0,
+	rating_img_url_small: '',
+	url: ''
 });
 
 // Places view
@@ -22,10 +27,26 @@ RFZ.page.PlaceView = Backbone.View.extend({
 		this.render();
 	},
 	render: function() {
+		//console.log('Here');
+		
 		var templateData = {
 			name: this.model.get('name'),
-			address: this.model.get('address')
+			category: this.model.get('categories')[0][0],
+			address: this.model.get('location').address,
+			city: this.model.get('location').city,
+			state: this.model.get('location').state_code,
+			postal_code: this.model.get('location').postal_code,
+			image_url: this.model.get('image_url'),
+			yelp_rating: this.model.get('rating_img_url_small')
 		}
+		
+		var myLatlng = new google.maps.LatLng(this.model.get('location').coordinate.latitude, this.model.get('location').coordinate.longitude);
+		
+		var marker = new google.maps.Marker({
+			position: myLatlng,
+			map: RFZ.page.map,
+			title: this.model.get('name')
+		});
 		
 		$('#place-tmpl').tmpl(templateData).appendTo(this.el);
 	},
@@ -35,9 +56,12 @@ RFZ.page.PlaceView = Backbone.View.extend({
 		if($(event.target).hasClass('voted')) {
 			$.ajax({
 				url: '/api/vote/del',
+				data: {
+					person_id: 1,
+					place_id: this.model.get('id')
+				},
 				type: 'POST',
 				success: function(result) {
-					console.log(result);
 					if(result === 0) {
 						// Vote removed
 						$(event.target).text('Add vote').removeClass('voted');
@@ -49,9 +73,12 @@ RFZ.page.PlaceView = Backbone.View.extend({
 		} else {
 			$.ajax({
 				url: '/api/vote/add',
+				data: {
+					person_id: 1,
+					place_id: this.model.get('id')
+				},
 				type: 'POST',
 				success: function(result) {
-					console.log(result);
 					if(result === 0 || result === 1) {
 						// Vote added or exists
 						$(event.target).text('Remove vote').addClass('voted');
@@ -64,22 +91,55 @@ RFZ.page.PlaceView = Backbone.View.extend({
 	}
 });
 
+RFZ.page.todaysVote = function() {
+	$('#jsGetVotedPlace').click(function(event) {
+		event.preventDefault();
+		
+		$.ajax({
+			url: '/api/select',
+			success: function(result) {
+				console.log(result);
+			}
+		});
+	});
+}
+
 //api/places
 RFZ.page.init = function() {
+	// Set up map
+	var myLatlng = new google.maps.LatLng(37.775, -122.4183333);
+	var myOptions = {
+		zoom: 14,
+		zoomControl: true,
+		zoomControlOptions: {
+			position: google.maps.ControlPosition.RIGHT_CENTER:
+		},
+		center: myLatlng,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	
+	RFZ.page.map = new google.maps.Map(document.getElementById("map"), myOptions);
+	
+	// Grab Places
 	$.ajax({
 		url: '/api/places',
 		success: function(result) {
 			$.each(result, function(index, value) {
-				console.log(value);
+				//console.log(value);
 				
 				var currentModel = new RFZ.page.Place({
 					id: value.id,
-					address: value.address,
+					categories: value.categories,
 					name: value.name,
-					yelp: value.yelp
+					image_url: value.image_url,
+					location: value.location,
+					rating: value.rating,
+					review_count: value.review_count,
+					rating_img_url_small: value.rating_img_url_small,
+					url: value.url
 				});
 				
-				var currentEl = $('<li/>').appendTo($('#places'));
+				var currentEl = $('<li/>', { class: 'clearfix' }).appendTo($('#places'));
 				
 				new RFZ.page.PlaceView({
 					el: currentEl,
@@ -88,6 +148,9 @@ RFZ.page.init = function() {
 			});
 		}
 	});
+	
+	// Set up other events
+	RFZ.page.todaysVote();
 }
 
 // No document.ready needed
